6351fb7664ad0637ecd13996a4c51a7d849816d1
kevinivanchretien <ivanvimke@gmail.com>
2022-10-09 13:03:22 -0300

    crear el espacio

---
+++ Parcial/src/Parcial.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Parcial.c
+ Author      : Kevin
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!! A Darle Atomos!!!"); /* prints !!! A Darle Atomos!!! */
+	return EXIT_SUCCESS;
+}
